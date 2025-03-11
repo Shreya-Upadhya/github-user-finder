eb2a62f0cd4883a276951d5acb07881ee6f25fbb
Shreya Upadhya <147341463+Shreya-Upadhya@users.noreply.github.com>
2025-03-11 16:09:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GithubSearch from './Components/GitHubSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubSearch />
+    </div>
+  );
+}
+
+export default App;
